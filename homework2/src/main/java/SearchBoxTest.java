8c11e9b245591a674daed9a87e1edbabf7fd9f8a
yavuzarslann <yavuz-arslan98@hotmail.com>
2022-07-02 19:51:15 +0300

    homework2 is done

---
+++ homework2/src/main/java/SearchBoxTest.java
@@ -0,0 +1,12 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SearchBoxTest {
+    public static void main(String[] args) {
+
+        //  TEST KODLARI TEST KLASÖRÜ ALTINDAKİ JAVA KLASÖRÜNÜN İÇİNDEDİR.
+
+    }
+}
